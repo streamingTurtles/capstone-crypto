6335a41da8c95e860cd588c26bce2336d851b92b
Peter Cardillo <pcardillo@streamingturtles.com>
2021-09-08 12:07:36 -0400

    changed React default port to 3006 to avoid other localhost servers running on my Mac | beginning to add the models for my postgres db | testing the free coinGecko API to retreive crypto curriency data

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require("express")
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
+
 
 // middleware used for cross domain and express that's interacting with the frontend
 app.use(cors());  // allow our server be accessible to any domain requesting data from the browser
 app.use(express.json()); // access to the request.body object to get JSON data
+
+
+// ROUTES
+
+// create price
+
+// get prices
+
+// get a current price
+
+// update a price
+
+// delete a price
+
+
 
 
 app.listen(5000, () => {
