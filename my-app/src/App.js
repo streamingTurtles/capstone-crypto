73fd787085aff89842ffefcf170776145582aa1c
Peter Cardillo <pcardillo@streamingturtles.com>
2021-09-08 19:45:19 -0400

    did front end file restructuring, streamlining the folders & files to prep for front end view JSON data from coingecko | JSON now accuratly returns the top 10 crypto stocks data (to be curated for view and db | added necessary bootstrap for react libraries

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Cryptos/app.css'
+import Logos from './Cryptos/Logos'
+import CoingeckoAPI from './Cryptos/Coingecko'
+
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Testing get pricing data for crypto curriencies using coingecko free API
-        </p>
-        <a
-          className="App-link"
-          // href="https://reactjs.org"
-          href="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      <Logos/>
+      <CoingeckoAPI/>
 
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {/* Learn React */}
-          click Here to get the TOP 10 Crypto Curriencies
-        </a>
-
-      </header>
     </div>
-  );
+  )
+
+
+
+
+
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
