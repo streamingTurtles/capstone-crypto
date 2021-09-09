633a9e60d25871bb0e358d1ab53837284ea6d21b
Peter Cardillo <pcardillo@streamingturtles.com>
2021-09-09 18:38:34 -0400

    implemented coinGecko API into React for 10 crypto stocks that populate the view with standard crypto data.  Created Coingecko, Logos components using reactstrap for view & axios for async API call to coinGecko API

--- my-app/src/Cryptos/Logos.js
+++ my-app/src/Cryptos/Logos.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const Logos = () => {
     return(
-        <>
-           
-           
-           
-        </>
+       <>
+            <Container>
+                <Row>
+                    <Col md= "12" className="text-center mt-">
+                        <h1>Top 10 Capto-Crypto</h1>
+                    </Col>
+
+                </Row>
+            </Container>           
+       </>
     )
 };
 
