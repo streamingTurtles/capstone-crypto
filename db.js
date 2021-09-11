fe75e599cec9c01da77ee60d51bb11f6c7d88b93
Peter Cardillo <pcardillo@streamingturtles.com>
2021-09-11 19:23:23 -0400

    updated db.js, schema.sql & server.js files - see notes for further detail

--- db.js
+++ db.js
@@ -3,8 +3,9 @@ const Pool = require("pg").Pool;   // using Pool, allows us to run queries with 
 const pool = new Pool({
     user: "postgres",
     psssword: "root",
+    host: "localhost",
     port: 5432,
-    database: "prices"
+    database: "crypto_db"
 });
 
 module.exports = pool;
--- db/schema.sql
+++ db/schema.sql
@@ -5,46 +5,68 @@ CREATE DATABASE crypto_db;
 \c crypto_db;
 
 
--- crypto ticker symbol & icon 
-CREATE TABLE cryptos (
-  id SERIAL PRIMARY KEY,
+-- crypto name
+CREATE TABLE crypto_name (
+  crypto_name_id SERIAL PRIMARY KEY,
+  name VARCHAR(10) NOT NULL
+);
+
+
+-- crypto symbol
+CREATE TABLE crypto_symbol (
+  crypto_symbol_id SERIAL PRIMARY KEY,
   name VARCHAR(10) NOT NULL,
-  icon CHAR(1)  
+  icon CHAR(1),
+  crypto_name_id INTEGER REFERENCES crypto_name(crypto_name_id) ON DELETE SET NULL
+
 );
 
 
+-- crypto rank
+CREATE TABLE crypto_rank (
+  crypto_rank_id SERIAL PRIMARY KEY,
+  rank INTEGER NOT NULL,
+  crypto_name_id INTEGER REFERENCES crypto_name(crypto_name_id) ON DELETE SET NULL 
+);
+
+
+
+
+
+
+-- 
 -- crypto yearly HI price 
-CREATE TABLE prices_year_hi(
-    id SERIAL PRIMARY KEY,
-    hi, NUMERIC(6, 2) NOT NULL,
-    crypto_id INTEGER REFERENCES cryptos(id) ON DELETE SET NULL
-);
-
-
+-- CREATE TABLE prices_year_hi(
+    -- id SERIAL PRIMARY KEY,
+    -- hi, NUMERIC(6, 2) NOT NULL,
+    -- id INTEGER REFERENCES crypto_name_id(id) ON DELETE SET NULL
+-- );
+-- 
+-- 
 -- crypto yearly LOW price 
-CREATE TABLE prices_year_low(
-    id SERIAL PRIMARY KEY,
-    low, NUMERIC(6, 2) NOT NULL,
-    crypto_id INTEGER REFERENCES cryptos(id) ON DELETE SET NULL
-);
-
-
+-- CREATE TABLE prices_year_low(
+    -- id SERIAL PRIMARY KEY,
+    -- low, NUMERIC(6, 2) NOT NULL,
+    -- id INTEGER REFERENCES crypto_name_id(id) ON DELETE SET NULL
+-- );
+-- 
+-- 
 -- crypto yearly CURRENT price 
-CREATE TABLE prices_year_current(
-    id SERIAL PRIMARY KEY,
-    current, NUMERIC(6, 2) NOT NULL,
-    crypto_id INTEGER REFERENCES cryptos(id) ON DELETE SET NULL
-);
-
-
+-- CREATE TABLE prices_year_current(
+    -- id SERIAL PRIMARY KEY,
+    -- current, NUMERIC(6, 2) NOT NULL,
+    -- id INTEGER REFERENCES crypto_name_id(id) ON DELETE SET NULL
+-- );
+-- 
+-- 
 -- buy sell recomendation 
-CREATE TABLE recomendation (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  review_text TEXT NOT NULL,
-  rating SMALLINT CHECK (rating BETWEEN 1 AND 5),
-  crypto_id INTEGER NOT NULL REFERENCES cryptos(id) ON DELETE CASCADE
-);
+-- CREATE TABLE recomendation (
+  -- id SERIAL PRIMARY KEY,
+  -- title VARCHAR(100) NOT NULL,
+  -- review_text TEXT NOT NULL,
+  -- rating SMALLINT CHECK (rating BETWEEN 1 AND 5),
+  -- id INTEGER NOT NULL REFERENCES crypto_name_id(id) ON DELETE CASCADE
+-- );
 
 
 
