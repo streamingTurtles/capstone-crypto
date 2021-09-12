b94c576cab776b9d77924a31fd1a9381d9de5600
Peter Cardillo <pcardillo@streamingturtles.com>
2021-09-12 14:56:10 -0400

    added form in front end that allows user to enter a crypto name to track that submits to my crypto_db via app.post route in <server.js> file

--- my-app/src/Cryptos/InputCrypto.js
+++ my-app/src/Cryptos/InputCrypto.js
@@ -1,9 +1,40 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 
 const InputCrypto = () => {
+
+    // setup state to add, track user input
+    const [name, addCryptoName] = useState("");
+    
+    // submit the form so we can send the data to our postgres db
+    const onSubmitForm = async e => {
+        e.preventDefault();  // prevent page refresh
+        try {
+          const body = { name }  ;  // name destructure of our crypto_name table in crypto_db
+          const response = await fetch("http://localhost:5000/createcrypto", {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(body)
+          });
+          console.log(response); // see the users input in express server console
+            
+        } catch (err) {
+            console.error(err.message);
+            
+        }
+    }
+
+
     return <Fragment>
-        <h1> Track your Crypto's:</h1>
+        <h1 className="text-center"> Track your Crypto's:</h1>
+        <form className="d-flex" onSubmit={onSubmitForm}>
+            <input type="text" 
+            className="form-control"
+            value={name}
+            onChange={e => addCryptoName(e.target.value)}
+            />
+            <button className="btn btn-success">Add Crypto</button>
+        </form>
     </Fragment>
 };
 
