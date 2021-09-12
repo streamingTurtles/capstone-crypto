2d1874d2e88586b84366338edf692b446e0c05e1
Peter Cardillo <pcardillo@streamingturtles.com>
2021-09-12 13:23:33 -0400

    continue building out frontEnd, now with incorporating backend RESTful API's that interact with our postgres db

---
+++ my-app/src/Cryptos/InputCrypto.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+
+const InputCrypto = () => {
+    return <Fragment>
+        <h1> Track your Crypto's:</h1>
+    </Fragment>
+};
+
+export default InputCrypto;
+
