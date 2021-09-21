a420d4ba849fe8f0d572432f15a7ed587b439c60
Peter Cardillo <pcardillo@streamingturtles.com>
2021-09-20 22:56:04 -0400

    updated db.js credentials to match the .env file

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool;   // using Pool, allows us to run queries with postgres
 
+// const pool = new Pool({
+//     user: "postgres",
+//     psssword: "root",
+//     host: "localhost",
+//     port: 5432,
+//     database: "crypto_db"
+// });
+
 const pool = new Pool({
-    user: "postgres",
-    psssword: "root",
-    host: "localhost",
-    port: 5432,
-    database: "crypto_db"
+    PGUSER: "postgres",
+    PGPASSWORD: "root",
+    PGHOST: "localhost",
+    PGPORT: 5432,
+    PGDATABASE: "crypto_db"
 });
 
 module.exports = pool;
