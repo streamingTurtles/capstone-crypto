21d0962a856f8fbf665d59eefdc26ccf638cd2d3
Peter Cardillo <pcardillo@streamingturtles.com>
2021-09-21 17:22:46 -0400

    changing user & pass for postgres to get working on deployment

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Pool = require("pg").Pool;   // using Pool, allows us to run queries with postgres
 
+// This config works with local Mac
 // const pool = new Pool({
 //     user: "postgres",
 //     psssword: "root",
@@ -9,13 +10,40 @@ const Pool = require("pg").Pool;   // using Pool, allows us to run queries with 
 // });
 
 
+// Doesn't work
+// const pool = new Pool({
+//     PGUSER: "crypto_user",
+//     PGPASSWORD: "password",
+//     PGHOST: "localhost",
+//     PGPORT: 5432,
+//     PORT:5000,
+//     PGDATABASE: "crypto_db"
+// });
+
+
 const pool = new Pool({
-    PGUSER: "crypto_user",
-    PGPASSWORD: "password",
-    PGHOST: "localhost",
-    PGPORT: 5432,
-    PORT:5000,
-    PGDATABASE: "crypto_db"
+    user: "crypto_user",
+    psssword: "password",
+    host: "localhost",
+    port: 5432,
+    database: "crypto_db"
 });
 
+
 module.exports = pool;
+
+
+
+
+// PREVIOUS .env
+// ENCRYPTION_KEY=1234 
+
+// PGHOST=localhost
+// PGUSER=crypto_user
+// PGDATABASE=crypto_db
+// PGPASSWORD=root
+// PGPORT=5432
+// REDPORT=6379
+// PORT=5000
+
+// my-app/.eslintcache
